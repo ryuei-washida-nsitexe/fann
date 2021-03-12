46844bc093b1c681733611080e9c724847acec05
NSITEXE WASHIDA Ryuei <ryuei_washida@scip.local>
2021-03-12 22:37:52 +0900

    Concatinating traing and run

--- examples/build_runtest.sh
+++ examples/build_runtest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # make sure the script fails if there is an error
 set -e
-echo "Build for runtest ..."
-make xor_train
-make xor_test
-make xor_test_fixed
+echo "Make xor_test_seq ..."
+make xor_test_seq
